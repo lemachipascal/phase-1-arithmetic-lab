1e6d805376e8ee79e54c1bc7b625f71cd7539876
Lemachipascal-dev <lemachijo421@gmail.com>
2024-07-03 15:49:48 +0300

    lab1

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+var multiply = num1 * num2;
+
+console.log(multiply); 
+
+var random = Math.floor (Math.random()*10) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 4;
+var mod = num3 / num4;
+console.log(mod);
+
+
+var max = Math.max(20, 10, 5);
+console.log(max);
